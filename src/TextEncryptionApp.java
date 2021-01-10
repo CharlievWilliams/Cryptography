c8f6397af1af5bd77ce649176b349d1705f86dbc
Charlie Williams <charliewilliamsweb@gmail.com>
2021-01-10 19:09:15 +0000

    Create GUI for encryption app

--- src/TextEncryptionApp.java
+++ src/TextEncryptionApp.java
@@ -1,2 +1,55 @@
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class TextEncryptionApp {
+
+    private JPanel mainPanel;
+    private JTextPane message1TextPane;
+    private JTextPane message2TextPane;
+    private JTextPane encryptedMessageTextPane;
+    private JTextArea encryptedMessageTextArea;
+    private JTextArea decryptedMessage1TextArea;
+    private JTextArea decryptedMessage2TextArea;
+    private JButton encryptButton;
+    private JButton decryptButton;
+    private JButton clearButton;
+
+    /**
+     * Description
+     *
+     * @param args description
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Verify Credit Card Number");
+        frame.setContentPane(new TextEncryptionApp().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Description
+     */
+    public TextEncryptionApp() {
+        clearButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                clearButtonClicked();
+            }
+        });
+    }
+
+    /**
+     * Description
+     */
+    private void clearButtonClicked() {
+        message1TextPane.setText("");
+        message2TextPane.setText("");
+        encryptedMessageTextArea.setText("");
+        encryptedMessageTextPane.setText("");
+        decryptedMessage1TextArea.setText("");
+        decryptedMessage2TextArea.setText("");
+    }
+
 }
