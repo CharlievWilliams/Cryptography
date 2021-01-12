26356737d70aef115a2a3784ad7cabdff706be3a
Charlie Williams <charliewilliamsweb@gmail.com>
2021-01-12 18:04:46 +0000

    I got it bloody working bond

--- src/BCHGeneratingAndCorrecting.java
+++ src/BCHGeneratingAndCorrecting.java
@@ -111,11 +111,8 @@ public class BCHGeneratingAndCorrecting {
         syndrome3 = SyndromeLibrary.calculateSyndrome3(inputArray);
         syndrome4 = SyndromeLibrary.calculateSyndrome4(inputArray);
 
-        if (syndrome1 == 10 || syndrome2 == 10 || syndrome3 == 10 || syndrome4 == 10) {
-            bchGeneratorTextArea.setText("Unusable Number");
-        } else {
-            bchGeneratorTextArea.setText("" + syndrome1 + syndrome2 + syndrome3 + syndrome4);
-        }
+        bchGeneratorTextArea.setText(syndrome1 + " " + syndrome2 + " " + syndrome3 + " " + syndrome4);
+
     }
 
     /**
@@ -136,23 +133,31 @@ public class BCHGeneratingAndCorrecting {
                         "One error. Corrected to " + Arrays.toString(result).replaceAll("\\[|\\]|,|\\s", "")
                 );
             } else {
-                int beforeTemp = ModuloLibrary.mod(q);
 
-                int temp = ModuloLibrary.squared(beforeTemp);
-                int temp2 = 4 * beforeTemp * r;
-                int temp3 = temp - temp2;
-                int temp4 = ModuloLibrary.mod(temp3);
-                int temp5 = ModuloLibrary.squareRoot(temp4);
-                int temp6 = ModuloLibrary.negative(q);
+                // TODO: Tidy this shit up
+                int partOne = ModuloLibrary.minus(0, q);
+                int partTwo = ModuloLibrary.squared(q);
+                int partThree = ModuloLibrary.mod(4 * p * r);
+                int partFour = ModuloLibrary.minus(partTwo, partThree);
+                if (ModuloLibrary.squareRoot(partFour) == 0) {
+                    bchDecoderResultsTextArea.setText("More than two errors");
+                    return;
+                }
+                int partFive = ModuloLibrary.squareRoot(partFour);
+                int partSix = ModuloLibrary.mod(partOne + partFive);
+                int partSeven = ModuloLibrary.mod(2 * p);
+                errorPosition1 = ModuloLibrary.inverse(partSix, partSeven);
 
-                errorPosition1 = ModuloLibrary.mod((temp6 + temp5) / 2 * p);
-                errorPosition2 = ModuloLibrary.mod((temp6 - temp5) / 2 * p);
+                int altPartSix = ModuloLibrary.minus(partOne, partFive);
+                errorPosition2 = ModuloLibrary.inverse(altPartSix, partSeven);
 
                 // Calculate positions and magnitudes
-                errorPosition1 = ModuloLibrary.mod((ModuloLibrary.negative(q) + ModuloLibrary.squareRoot(ModuloLibrary.squared(q) - 4 * p * r)) / 2 * p);
-                errorPosition2 = ModuloLibrary.mod((ModuloLibrary.negative(q) - ModuloLibrary.squareRoot(ModuloLibrary.squared(q) - 4 * p * r)) / 2 * p);
-                errorMagnitude2 = ModuloLibrary.mod((errorPosition1 * syndrome1 - syndrome2) / (errorPosition1 - errorPosition2));
-                errorMagnitude1 = ModuloLibrary.mod(syndrome1 - errorMagnitude2);
+                // TODO: Also tidy this bollocks up
+                int orderOne = ModuloLibrary.mod(errorPosition1 * syndrome1);
+                int orderTwo = ModuloLibrary.minus(orderOne, syndrome2);
+                int orderThree = ModuloLibrary.minus(errorPosition1, errorPosition2);
+                errorMagnitude2 = ModuloLibrary.inverse(orderTwo, orderThree);
+                errorMagnitude1 = ModuloLibrary.minus(syndrome1, errorMagnitude2);
 
                 if (errorPosition1 == 0 || errorPosition2 == 0) { // More than Double error
                     bchDecoderResultsTextArea.setText("More than two errors");
@@ -185,7 +190,7 @@ public class BCHGeneratingAndCorrecting {
      * @return Description
      */
     private int[] resolveSingleError(int errorPosition, int errorMagnitude) {
-        inputArray[errorPosition - 1] = inputArray[errorPosition - 1] - errorMagnitude; // TODO: What if it's +
+        inputArray[errorPosition - 1] = ModuloLibrary.mod(inputArray[errorPosition - 1] - errorMagnitude);
         return inputArray;
     }
 
@@ -199,8 +204,8 @@ public class BCHGeneratingAndCorrecting {
      * @return Description
      */
     private int[] resolveDoubleError(int errorPosition1, int errorPosition2, int errorMagnitude1, int errorMagnitude2) {
-        inputArray[errorPosition1 - 1] = inputArray[errorPosition1 - 1] - errorMagnitude1;
-        inputArray[errorPosition2 - 1] = inputArray[errorPosition2 - 1] - errorMagnitude2;
+        inputArray[errorPosition1 - 1] = ModuloLibrary.mod(inputArray[errorPosition1 - 1] - errorMagnitude1);
+        inputArray[errorPosition2 - 1] = ModuloLibrary.mod(inputArray[errorPosition2 - 1] - errorMagnitude2);
         return inputArray;
     }
 
--- src/ModuloLibrary.java
+++ src/ModuloLibrary.java
@@ -8,39 +8,51 @@ public class ModuloLibrary {
      */
     public static int mod(int input) {
         int result = input % 11;
-        if (result < 0)
+        if (result < 0) {
             result += 11;
+        }
         return result;
     }
 
     /**
      * Description
      *
-     * @param number Description
+     * @param negative Description
      * @return Description
      */
-    public static int negative(int number) {
-        switch (number) {
+    public static int minus(int number, int negative) {
+        return mod(number - negative);
+    }
+
+    /**
+     * Description
+     *
+     * @param number  Description
+     * @param divisor Description
+     * @return Description
+     */
+    public static int inverse(int number, int divisor) {
+        switch (divisor) {
             case 1:
-                return 10;
+                return mod(number);
             case 2:
-                return 9;
+                return mod(number * 6);
             case 3:
-                return 8;
+                return mod(number * 4);
             case 4:
-                return 7;
+                return mod(number * 3);
             case 5:
-                return 6;
+                return mod(number * 9);
             case 6:
-                return 5;
+                return mod(number * 2);
             case 7:
-                return 4;
+                return mod(number * 8);
             case 8:
-                return 3;
+                return mod(number * 7);
             case 9:
-                return 2;
+                return mod(number * 5);
             case 10:
-                return 1;
+                return mod(number * 10);
             default:
                 return 0;
         }
@@ -53,25 +65,7 @@ public class ModuloLibrary {
      * @return Description
      */
     public static int squared(int number) {
-        switch (number) {
-            case 1:
-            case 10:
-                return 1;
-            case 2:
-            case 9:
-                return 4;
-            case 3:
-            case 8:
-                return 9;
-            case 4:
-            case 7:
-                return 5;
-            case 5:
-            case 6:
-                return 3;
-            default:
-                return 0;
-        }
+        return mod(number * number);
     }
 
     /**
